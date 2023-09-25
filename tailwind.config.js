a8b7bf9ee77b37d38c3c1c0a20d3133bc96baebe
Abhishek <shettyabhishek829@yahoo.in>
2023-09-25 11:07:54 +0530

    samples

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
-  theme: {
+  /*theme: {
     extend: {},
-  },
+  },*/
   plugins: [],
+  theme: {
+      extend: {
+          transitionDuration: {
+            2000: '2000ms'
+          },
+          animation: {
+            'spin-slow': 'spin 3s linear infinite'
+          }
+      }
+  }
 }
 
